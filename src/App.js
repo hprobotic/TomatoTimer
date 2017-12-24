d71d74f00d3cf3960c11170dec732cbb34d3330f
Tien Manh Do <domanhtien2011@gmail.com>
2017-12-24 12:33:26 +0700

    Moving navbar to separate component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 import { Button, Segment, Container, Menu, Item, Grid } from 'semantic-ui-react';
 // import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Segment inverted color='black' className='homepage-segment'>
-          <Container>
-            <Menu inverted pointing secondary>
-              <h1>Tomato Timer</h1>
-              <Item className='right'>
-                <a href="#">FAQ</a>
-                <a href="#" className='homepage-setting'>Settings</a>
-              </Item>
-            </Menu>
-          </Container>
-        </Segment>
+        <NavBar/>
         <Grid centered>
           <Button.Group>
             <Button color='twitter' className='main-btn'>Pomodoro</Button>
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Button, Segment, Container, Menu, Item, Grid } from 'semantic-ui-react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <Segment inverted color='black' className='homepage-segment'>
+        <Container>
+          <Menu inverted pointing secondary>
+            <h1>Tomato Timer</h1>
+            <Item className='right'>
+              <a href="#">FAQ</a>
+              <a href="#" className='homepage-setting'>Settings</a>
+            </Item>
+          </Menu>
+        </Container>
+      </Segment>
+    );
+  }
+}
+
+export default Navbar;
