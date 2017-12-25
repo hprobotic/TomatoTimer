13f763c19e9c2181c4362e66190f37442741b504
Tien Manh Do <domanhtien2011@gmail.com>
2017-12-25 19:11:29 +0700

    Remove unnecessary container

--- src/containers/MainButtons.js
+++ src/containers/MainButtons.js
@@ -2,18 +2,60 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Grid, Button } from 'semantic-ui-react';
-import { startCountdown } from '../actions/index';
+import _ from 'lodash';
+import { startCountdown, stopCountdown, resetCountdown } from '../actions/index';
 
 
 class MainButtons extends Component {
+
+  // Set the date we're counting down to
+  // countDown() {
+  //   let now = new Date();
+  //   let next_15_minutes = now.setMinutes(now.getMinutes() + 15)
+  //   console.log(next_15_minutes);
+  //   let x = setInterval(() => {
+  //     // Find the distance between now an the count down date
+  //     let distance = next_15_minutes - now;
+  //     console.log('called');
+  //     console.log(distance);
+  //     // Time calculations for minutes and seconds
+  //     this.setState({
+  //       minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+  //       seconds: Math.floor((distance % (1000 * 60)) / 1000)
+  //     });
+
+
+  // If the count down is finished, write some text
+  //   if (distance < 0) {
+  //     clearInterval(x);
+  //   }
+  // }, 1000);
+  // }
+
   render() {
+    console.log(_.keys(this.props));
+    console.log(_.values(this.props));
+    console.log(this.props);
     return (
-      <Grid centered columns={3}>
-        <Button color='green' size='massive' onClick={() => this.props.startCountdown()}>Start</Button>
-        <Button color='red' size='massive'>Stop</Button>
-        <Button color='gray' size='massive'>Reset</Button>
-      </Grid>
-    );
+      <div>
+        <Grid textAlign='center'>
+          <h1 className='time-countdown'>{this.props.time.startedAt}</h1>
+        </Grid>
+        <Grid centered columns={3}>
+          <Button color='green' size='massive' onClick={() => this.props.startCountdown()}>Start</Button>
+          <Button color='red' size='massive' onClick={() => this.props.stopCountdown()}>Stop</Button>
+          <Button color='gray' size='massive' onClick={() => this.props.resetCountdown()}>Reset</Button>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+
+function mapStateToProps(state) {
+  // Whatever is returned from here will show up as props inside of MainFeatures
+  return {
+    time: state.time
   }
 }
 
@@ -26,4 +68,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 // Promote MainButtons from a component to a container - it needs to know about this new dispatch method, countDown. Make it available as a prop.
-export default connect(null, mapDispatchToProps)(MainButtons)
+export default connect(mapStateToProps, mapDispatchToProps)(MainButtons)
