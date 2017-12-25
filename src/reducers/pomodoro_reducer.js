a938baaac24c621f15c6bd9b8416ccd7ca5781a4
Tien Manh Do <domanhtien2011@gmail.com>
2017-12-26 00:41:55 +0700

    Changing pomodoro default value

--- src/reducers/pomodoro_reducer.js
+++ src/reducers/pomodoro_reducer.js
@@ -1,9 +1,5 @@
 import { START_COUNTDOWN, STOP_COUNTDOWN, RESET_COUNTDOWN, ON_TICK } from '../actions/index';
-<<<<<<< HEAD
 const POMODORO = 25
-=======
-const POMODORO = 0.1
->>>>>>> 016f4247a7cbcb133864ab12106e4a30d0e77384
 
 const initialState = {
   pomodoro: {
