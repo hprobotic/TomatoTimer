977c032adfc6d8a0a04de6c7e1746b142fc66913
John Pham <hprobotic@gmail.com>
2017-12-28 03:27:13 +0700

    [f] Update timer, circle and background

--- src/App.js
+++ src/App.js
@@ -14,11 +14,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="random-bg" />
         <div className="grid-container">
           <div className="item item-1">
             <div className="logo">
-              <span class="title">Tomato Timer</span>
-              <span class="description">
+              <span className="title">Tomato Timer</span>
+              <span className="description">
                 <span>100K+ </span> focus hours
               </span>
             </div>
@@ -32,11 +33,9 @@ class App extends Component {
             <Timer />
           </div>
           <div className="item item-5">5</div>
-          <div className="item item-6" />
           <div className="item item-7">
             <Quotes />
             <KeyboardShortcuts />
-            <Footer />
           </div>
         </div>
       </div>
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,239 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Grid, Button } from 'semantic-ui-react';
+import moment from 'moment';
+import { defaultBreak, shortBreak, longBreak } from '../../actions';
+import _ from 'lodash';
+import './Timer.css';
+
+const PROGRESS_CIRCUMFERENCE = 992.743278534;
+
+class MainButtons extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerRunning: false,
+      currentSeconds: 1500,
+      timerStatus: 'init'
+    };
+  }
+
+  componentWillMount() {
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentSeconds: this.props.seconds
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+    if (this.props.seconds !== nextProps.seconds) {
+      this.onResetButtonPressed();
+      this.setState({
+        currentSeconds: nextProps.seconds
+      });
+    }
+  }
+
+  tick() {
+    this.setState(
+      prevState => ({
+        currentSeconds: prevState.currentSeconds - 1
+      }),
+      () => {
+        if (this.state.currentSeconds === 0) this.resetInterval();
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  handleKeyDown = e => {
+    if (e.altKey) {
+      switch (e.which) {
+        case 80:
+          this.props.defaultBreak();
+          break;
+        case 83:
+          console.log('hello');
+          this.props.shortBreak();
+          break;
+        case 76:
+          this.props.longBreak();
+          break;
+        case 82:
+          this.onStopButtonPressed();
+          break;
+        default:
+          break;
+      }
+    } else {
+      if (e.which == 32) {
+        console.log('Go space');
+        if (this.state.timerStatus === 'running') {
+          this.onStopButtonPressed();
+        } else {
+          this.onStartButtonPressed();
+        }
+      }
+    }
+  };
+
+  resetInterval() {
+    clearInterval(this.timerID);
+  }
+
+  restartInterval() {
+    clearInterval(this.timerID);
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+
+  format = seconds => {
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = Math.floor((seconds % 3600) % 60);
+    let timeFormated = (m < 10 ? '0' : '') + m + ':' + (s < 10 ? '0' : '') + s;
+    return timeFormated;
+  };
+
+  onStartButtonPressed = () => {
+    if (this.state.timerStatus === 'running') return;
+    this.restartInterval();
+    this.setState({
+      timerStatus: 'running'
+    });
+  };
+
+  onPauseButtonPressed = () => {
+    this.resetInterval();
+    this.setState({
+      timerStatus: 'pause'
+    });
+  };
+
+  onResumeButtonPressed = () => {
+    if (this.state.timerStatus === 'running') return;
+    this.restartInterval();
+    this.setState({
+      timerStatus: 'running'
+    });
+  };
+
+  onStopButtonPressed = () => {
+    if (!this.state.timerRunning) return;
+    this.resetInterval();
+    this.setState({
+      timerStatus: 'pause'
+    });
+  };
+
+  onResetButtonPressed = () => {
+    this.resetInterval();
+    this.setState({
+      timerStatus: 'init',
+      currentSeconds: this.props.seconds
+    });
+  };
+
+  render() {
+    const { timerStatus, currentSeconds } = this.state;
+    const isCounting = timerStatus === 'running';
+    const currentCircleProgress =
+      PROGRESS_CIRCUMFERENCE * currentSeconds / this.props.seconds;
+    return (
+      <div className="timer">
+        <h1 className={`countdown ${isCounting ? 'animated' : 'stop'}`}>
+          <svg
+            viewBox="0 0 320 320"
+            width="320"
+            height="320"
+            className="circle-svg"
+          >
+            <circle
+              class="animate"
+              id="path"
+              cx="160"
+              cy="160"
+              r="158"
+              stroke="#fff"
+              fill="none"
+              strokeWidth="4"
+              strokeLinecap="round"
+              stroke-dashoffset={currentCircleProgress}
+            />
+          </svg>
+          <div className="countdown-circle" />
+          <div className="countdown-circle circle-2" />
+          <div className="countdown-circle circle-3" />
+          <div className="countdown-circle circle-4" />
+          {this.format(this.state.currentSeconds)}
+        </h1>
+        <Grid centered columns={3}>
+          {timerStatus === 'init' && (
+            <Button
+              size="huge"
+              color="green"
+              onClick={() => this.onStartButtonPressed()}
+            >
+              Start
+            </Button>
+          )}
+          {timerStatus === 'running' && (
+            <Button
+              basic
+              size="huge"
+              inverted
+              onClick={() => this.onPauseButtonPressed()}
+            >
+              Pause
+            </Button>
+          )}
+          {timerStatus === 'pause' && (
+            <Button
+              size="huge"
+              color="red"
+              onClick={() => this.onResumeButtonPressed()}
+            >
+              Resume
+            </Button>
+          )}
+          {timerStatus === 'pause' && (
+            <Button
+              size="huge"
+              color="grey"
+              onClick={this.onResetButtonPressed}
+            >
+              Reset
+            </Button>
+          )}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    seconds: state.pomodoro.seconds
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      shortBreak: shortBreak,
+      longBreak: longBreak,
+      defaultBreak: defaultBreak
+    },
+    dispatch
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainButtons);
