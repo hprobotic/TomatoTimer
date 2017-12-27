5909d5ff5ac784d392c99ec1492783b6b9ffbeeb
John Pham <hprobotic@gmail.com>
2017-12-27 12:57:54 +0700

    [f] Add keyboard shortcut view

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import { Grid, Image } from 'semantic-ui-react';
 import '../App.css';
 import NavBar from './NavBar';
-import MainFeatures from '../components/MainFeatures';
+import MainFeatures from './MainFeatures';
 import MainButtons from '../containers/MainButtons';
+import KeyboardShortcuts from './KeyboardShortcuts';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <br />
-        <br />
-        <MainFeatures />
-        <br />
-        <br />
-        <MainButtons />
+        <Grid>
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <NavBar />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <MainFeatures />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <MainButtons />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={3}>
+            <Grid.Column>
+              <KeyboardShortcuts />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
