2fd2f31419de3beb4a8da4ed74feff67ea819284
van le <vanlecs09@gmail.com>
2017-12-28 18:07:24 +0700

    ~quote

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './Quotes.css';
 
-const Quotes = () => (
-  <div className="quotes">
-    <p className="quote">"Our of difficulties grow miracles"</p>
-    <p className="author">JEAN DE LA BRUYERE</p>
-  </div>
-);
+
+const QUOTE_URL = `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1`
+const TIME_UPDATE_QUOTE = 1000 * 10
+
+class Quotes extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: "Our of difficulties grow miracles",
+      title : "JEAN DE LA BRUYERE"
+    }
+  }
+
+  componentWillMount() {
+    let self = this;
+    setInterval(() => {
+      fetch(QUOTE_URL)
+      .then((data) => data.json())
+      .then((json) => {
+        self.setState({
+          content: json[0].content,
+          title: json[0].title
+        })
+      })
+    }, TIME_UPDATE_QUOTE);
+  }
+
+  render() {
+    return (
+      <div className="quotes">
+        <p className="quote">{this.state.content}</p>
+        <p className="author">{this.state.title}</p>
+      </div>
+    )
+  }
+}
 
 export default Quotes;
--- src/reducers/pomodoro_reducer.js
+++ src/reducers/pomodoro_reducer.js
@@ -1,42 +1,79 @@
-import {
-  START_COUNTDOWN,
-  STOP_COUNTDOWN,
-  RESET_COUNTDOWN,
-  ON_TICK,
-  SHORT_BREAK,
-  LONG_BREAK,
-  DEFAULT_BREAK
-} from '../actions';
+import { START_COUNTDOWN, STOP_COUNTDOWN, RESET_COUNTDOWN, ON_TICK, SHORT_BREAK, LONG_BREAK} from '../actions/index';
 
-const DEFAULT_BREAK_SECS = 1500;
-const SHORT_BREAK_SECS = 300;
-const LONG_BREAK_SECS = 600;
+const POMODORO = 25
+const SECONDS = '00'
+const SHORT_BREAK_MIN = 5
+const LONG_BREAK_MIN = 10
 
 const initialState = {
-  seconds: DEFAULT_BREAK_SECS
+  pomodoro: {
+    display: {
+      minutes: POMODORO,
+      seconds: SECONDS
+    },
+    ticking: false
+  }
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case ON_TICK:
       console.log('just went through case ON_TICK...');
       return {
-        ...state,
-        seconds: action.seconds
+        pomodoro: {
+          display: {
+            minutes: action.nextMinute,
+            seconds: action.nextSecond
+          }
+        }
       };
-    case (RESET_COUNTDOWN, DEFAULT_BREAK):
-      return initialState;
-    case SHORT_BREAK:
-      console.log('STATE: ', state);
+    case RESET_COUNTDOWN:
+    console.log('just went through case RESET_COUNTDOWN...');
       return {
-        ...state,
-        seconds: SHORT_BREAK_SECS
+        ...initialState
       };
+    case START_COUNTDOWN:
+      console.log('just went through case START_COUNTDOWN...');
+      return {
+        pomodoro: {
+          display: {
+            minutes: state.pomodoro.display.minutes,
+            seconds: state.pomodoro.display.seconds
+          },
+          ticking: action.ticking
+        }
+      };
+    case STOP_COUNTDOWN:
+      console.log('just went through case STOP_COUNTDOWN...');
+      return {
+        pomodoro: {
+          display: {
+            minutes: state.pomodoro.display.minutes,
+            seconds: state.pomodoro.display.seconds
+          },
+          ticking: action.ticking
+        }
+      }
+    case SHORT_BREAK:
+      return {
+        pomodoro: {
+          display: {
+            minutes: SHORT_BREAK_MIN,
+            seconds: SECONDS
+          },  
+          ticking: false
+        }
+      }
     case LONG_BREAK:
       return {
-        ...state,
-        seconds: LONG_BREAK_SECS
-      };
+        pomodoro: {
+          display: {
+            minutes: LONG_BREAK_MIN,
+            seconds: SECONDS
+          },
+          ticking: false
+        }
+      }
     default:
       return state;
   }
