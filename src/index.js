373866d9ef039b99e85a00813bb15342961a1478
John Pham <hprobotic@gmail.com>
2017-12-30 13:00:12 +0700

    [f] Change redux structure

--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducers from './reducers'
+import { createDevTools } from 'redux-devtools'
+import { persistStore, persistCombineReducers } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import storage from 'redux-persist/es/storage'
+import * as reducers from './reducers'
 import registerServiceWorker from './registerServiceWorker'
 import App from './App'
 import './index.css'
 
+const config = {
+  key: 'root',
+  storage
+}
+
+const reducer = persistCombineReducers(config, {
+  ...reducers
+})
+
+let store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+let persistor = persistStore(store)
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,2 @@
-import { combineReducers } from 'redux'
-import PomodoroReducer from './pomodoro_reducer'
-
-// Here is how the Application's state is generated
-const rootReducer = combineReducers({
-  pomodoro: PomodoroReducer
-})
-
-export default rootReducer
+import pomodoro from './pomodoroReducer'
+export { pomodoro }
